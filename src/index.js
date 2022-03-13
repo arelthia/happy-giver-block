3e297b5941e93a53690dfab2d886caa4d7370866
Arelthia Phillips <arelthia@pintopsolutions.com>
2022-03-12 23:17:57 -0600

    Add style controls to the block editor, description, update svg icon

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import "./index.scss"
-import {TextControl, Flex, FlexBlock, FlexItem,  __experimentalHStack as HStack, Button, Icon, CheckboxControl} from "@wordpress/components"
-
+import icons from "./icons"
+import {TextControl, Flex, FlexBlock, FlexItem,  __experimentalHStack as HStack, Button, Icon, CheckboxControl, PanelBody, PanelRow, ColorPicker} from "@wordpress/components"
+import {InspectorControls} from "@wordpress/block-editor"
 
 wp.blocks.registerBlockType("happygiverblock/happy-giver", {
 	title: "Happy Giver",
-	icon: "smiley",
+	icon: icons.block,
 	category: "common",
+	description: "Donation Form with customizeable amounts and purpose.", 
 	attributes: {
 		amounts: {type: "array", default: [""]},
 		purposes: {type: "array", default: [""]},
-		allowCustom: {type: "boolean"}
+		allowCustom: {type: "boolean"},
+		bgColor: {type: "string", default: "#F1F1F1"},
+		amountButtonColor: {type: "string", default: "#EEEADD"},
+		amountColor: {type: "string", default: "#FFFFFF"},
+		purposeButtonColor: {type: "string", default: "#EEEADD"},
+		purposeColor: {type: "string", default: "#FFFFFF"}
+	},
+	example: {
+		attributes: {
+			amounts: ["50", "500", "1000"],
+			purposes: ["Food", "Radio"],
+			allowCustom: true,
+			bgColor: "#F1F1F1",
+			amountButtonColor: "#EEEADD",
+			amountColor: "#FFFFFF",
+			purposeButtonColor: "#EEEADD",
+			purposeColor: "#FFFFFF"
+		}
 	},
 	edit: EditComponent,
 	save: function (props) {
@@ -35,14 +54,41 @@ function EditComponent (props) {
 	}
 
 	return (
-		<div className="happy-giver-edit-block">
+		<div className="happy-giver-edit-block" style={{backgroundColor: props.attributes.bgColor}}>
+			<InspectorControls>
+				<PanelBody title="Background Color" initialOpen={true}>
+					<PanelRow>
+						<ColorPicker color={props.attributes.bgColor} onChangeComplete={ theColor => props.setAttributes({bgColor: theColor.hex})} />
+					</PanelRow>
+				</PanelBody>
+				<PanelBody title="Amount Background Color" initialOpen={false}>
+					<PanelRow>
+						<ColorPicker color={props.attributes.amountButtonColor} onChangeComplete={ amtBg => props.setAttributes({amountButtonColor: amtBg.hex})} />
+					</PanelRow>
+				</PanelBody>
+				<PanelBody title="Amount Color" initialOpen={false}>
+					<PanelRow>
+						<ColorPicker color={props.attributes.amountColor} onChangeComplete={ amtColor => props.setAttributes({amountColor: amtColor.hex})} />
+					</PanelRow>
+				</PanelBody>
+				<PanelBody title="Purpose Background Color" initialOpen={false}>
+					<PanelRow>
+						<ColorPicker color={props.attributes.purposeButtonColor} onChangeComplete={ purpBg => props.setAttributes({purposeButtonColor: purpBg.hex})} />
+					</PanelRow>
+				</PanelBody>
+				<PanelBody title="Purpose Color" initialOpen={false}>
+					<PanelRow>
+						<ColorPicker color={props.attributes.purposeColor} onChangeComplete={ purpColor => props.setAttributes({purposeColor: purpColor.hex})} />
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
 			<p>Amounts</p>
 			<HStack >
 			{props.attributes.amounts.map(function (amount, index) {
 				return (
 
 					<Flex>
-						<FlexItem  className="single-amount" style={{textAlign: "center"}}>
+						<FlexItem  className="single-amount" style={{color: props.attributes.amountColor , backgroundColor: props.attributes.amountButtonColor , textAlign: "center"}}>
 							<TextControl className="single-amount-text" autoFocus={amount == undefined} value={amount}   onChange={newValue => {
 								const newAmounts = props.attributes.amounts.concat([])
 								newAmounts[index] = newValue
@@ -95,3 +141,4 @@ function EditComponent (props) {
 		</div>
 	)
 }
+
